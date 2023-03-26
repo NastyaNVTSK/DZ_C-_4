f5e49e5c7f5a563007c5dd7c0ddfa4cd0d69fdf5
Anastasia Novitskaya <nastasya11011995@gmail.com>
2023-03-26 19:11:38 +1200

    task_36

---
+++ task_36/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 50);
+}
+int Summ(int[] array)
+{
+    int sum = 0;
+    int i = 0;
+    while(i<array.Length)
+    {
+        sum += array[i];
+        i = i + 2;
+    }
+    return sum;
+}
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+        Console.Write($"{item} ");
+    Console.WriteLine();
+}
+int[] array = new int[4];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine($"Сумма элементов нечетных позиций: {Summ(array)}");
