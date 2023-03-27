3612fe5398a4d9e479495ab588fd4648687680d0
Anastasia Novitskaya <nastasya11011995@gmail.com>
2023-03-27 17:43:36 +1200

    task_38

---
+++ task_38/Program.cs
@@ -0,0 +1,30 @@
+﻿void FillArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().NextDouble();
+}
+double Diff(double[] array)
+{
+    int i = 1;
+     double min = array[0];
+     double max = array[0];
+    while(i<array.Length)
+    {
+        if(max<array[i])
+        max = array[i];
+         if(min>array[i])
+        min = array[i];
+        i++;
+    }
+    return max-min;
+}
+void PrintArray(double[] array)
+{
+    foreach (double item in array)
+        Console.Write($"{item} ");
+    Console.WriteLine();
+}
+double [] array = new double[4];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine($"Разница: {Diff(array)}");
